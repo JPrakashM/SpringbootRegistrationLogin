0bc7f5d3c89c75e3762fff0724980c9a2bb027c4
JPrakashM <jyotiinis18@gmail.com>
2023-06-01 07:25:31 +0000

    remove one row

--- src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
+++ src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
@@ -9,5 +9,4 @@ public class SpringBootRegistrationLoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRegistrationLoginApplication.class, args);
 	}
-
 }
